4fc1973901b27d7103027a51b61f3089e767132c
Luciano Quercia <luciano.quercia@gmail.com>
2023-02-25 16:04:29 +0000

    achievement category update + Stringer

--- chpp/type_achievement_category.go
+++ chpp/type_achievement_category.go
@@ -10,4 +10,23 @@ const (
 	AchievementCategoryMatches       AchievementCategory = 3
 	AchievementCategoryManager       AchievementCategory = 4
 	AchievementCategorySpecialAwards AchievementCategory = 5
+	AchievementCategorySupporter     AchievementCategory = 6
 )
+
+func (c AchievementCategory) String() string {
+	switch c {
+	case AchievementCategoryRanking:
+		return "ranking"
+	case AchievementCategoryTeam:
+		return "team"
+	case AchievementCategoryMatches:
+		return "matches"
+	case AchievementCategoryManager:
+		return "manager"
+	case AchievementCategorySpecialAwards:
+		return "special_awards"
+	case AchievementCategorySupporter:
+		return "supporter"
+	}
+	return "unknown achievement"
+}
