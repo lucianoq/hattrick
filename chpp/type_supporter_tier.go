dc691c21c672ea2103b03bb752feaeff269d0e1b
Luciano Quercia <luciano.quercia@gmail.com>
2023-02-25 16:17:53 +0000

    supporter tier update + Stringer

--- chpp/type_supporter_tier.go
+++ chpp/type_supporter_tier.go
@@ -5,9 +5,32 @@ type SupporterTier string
 
 // List of SupporterTier constants.
 const (
+	SupporterTierEmpty    SupporterTier = ""
 	SupporterTierNone     SupporterTier = "none"
 	SupporterTierSilver   SupporterTier = "silver"
 	SupporterTierGold     SupporterTier = "gold"
 	SupporterTierPlatinum SupporterTier = "platinum"
 	SupporterTierDiamond  SupporterTier = "diamond"
 )
+
+func (t SupporterTier) String() string {
+	switch t {
+	
+	case SupporterTierEmpty, SupporterTierNone:
+		return "none"
+
+	case SupporterTierSilver:
+		return "silver"
+
+	case SupporterTierGold:
+		return "gold"
+
+	case SupporterTierPlatinum:
+		return "platinum"
+
+	case SupporterTierDiamond:
+		return "diamond"
+	}
+
+	return "unknown"
+}
